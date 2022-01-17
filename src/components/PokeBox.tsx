caad790f694679fc4cf40a9e144416e04e77b832
Alberto Moura <56458241+AlberttMoura@users.noreply.github.com>
2022-01-17 15:40:31 -0300

    Exibição fracionada quase feito

--- src/components/PokeBox.tsx
+++ src/components/PokeBox.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react'
+/*import { useState, useEffect } from 'react'
 import axios from 'axios'
-
-interface Filter {
+*/
+/*interface Filter {
 	bug: boolean
 	dark: boolean
 	dragon: boolean
@@ -22,11 +22,9 @@ interface Filter {
 	water: boolean
 	intersection: boolean
 }
-
+*/
 interface PokeBoxProps {
-	name: string
-	url: string
-	filters: Filter
+	pokeInfo: Pokemon
 }
 
 interface Pokemon {
@@ -50,7 +48,7 @@ interface Pokemon {
 }
 
 export const PokeBox = (props: PokeBoxProps) => {
-	const [pokeInfo, setPokeInfo] = useState<Pokemon>()
+	//const [pokeInfo, setPokeInfo] = useState<Pokemon>()
 
 	const getColorType = (type?: string) => {
 		return type === 'grass'
@@ -92,238 +90,49 @@ export const PokeBox = (props: PokeBoxProps) => {
 			: 'white'
 	}
 
-	useEffect(() => {
-		axios
-			.get(props.url)
-			.then((res) => {
-				setPokeInfo(res.data)
-			})
-			.catch((err) => {
-				console.error(err)
-			})
-	}, [props.url])
-
-	const checkFilters = () => {
-		return props.filters.intersection
-			? props.filters.bug &&
-			  pokeInfo?.types[0]?.type.name !== 'bug' &&
-			  pokeInfo?.types[1]?.type.name !== 'bug'
-				? false
-				: props.filters.dark &&
-				  pokeInfo?.types[0]?.type.name !== 'dark' &&
-				  pokeInfo?.types[1]?.type.name !== 'dark'
-				? false
-				: props.filters.dragon &&
-				  pokeInfo?.types[0]?.type.name !== 'dragon' &&
-				  pokeInfo?.types[1]?.type.name !== 'dragon'
-				? false
-				: props.filters.electric &&
-				  pokeInfo?.types[0]?.type.name !== 'electric' &&
-				  pokeInfo?.types[1]?.type.name !== 'electric'
-				? false
-				: props.filters.fairy &&
-				  pokeInfo?.types[0]?.type.name !== 'fairy' &&
-				  pokeInfo?.types[1]?.type.name !== 'fairy'
-				? false
-				: props.filters.fighting &&
-				  pokeInfo?.types[0]?.type.name !== 'fighting' &&
-				  pokeInfo?.types[1]?.type.name !== 'fighting'
-				? false
-				: props.filters.fighting &&
-				  pokeInfo?.types[0]?.type.name !== 'fighting' &&
-				  pokeInfo?.types[1]?.type.name !== 'fighting'
-				? false
-				: props.filters.fire &&
-				  pokeInfo?.types[0]?.type.name !== 'fire' &&
-				  pokeInfo?.types[1]?.type.name !== 'fire'
-				? false
-				: props.filters.flying &&
-				  pokeInfo?.types[0]?.type.name !== 'flying' &&
-				  pokeInfo?.types[1]?.type.name !== 'flying'
-				? false
-				: props.filters.ghost &&
-				  pokeInfo?.types[0]?.type.name !== 'ghost' &&
-				  pokeInfo?.types[1]?.type.name !== 'ghost'
-				? false
-				: props.filters.grass &&
-				  pokeInfo?.types[0]?.type.name !== 'grass' &&
-				  pokeInfo?.types[1]?.type.name !== 'grass'
-				? false
-				: props.filters.ground &&
-				  pokeInfo?.types[0]?.type.name !== 'ground' &&
-				  pokeInfo?.types[1]?.type.name !== 'ground'
-				? false
-				: props.filters.ice &&
-				  pokeInfo?.types[0]?.type.name !== 'ice' &&
-				  pokeInfo?.types[1]?.type.name !== 'ice'
-				? false
-				: props.filters.normal &&
-				  pokeInfo?.types[0]?.type.name !== 'normal' &&
-				  pokeInfo?.types[1]?.type.name !== 'normal'
-				? false
-				: props.filters.poison &&
-				  pokeInfo?.types[0]?.type.name !== 'poison' &&
-				  pokeInfo?.types[1]?.type.name !== 'poison'
-				? false
-				: props.filters.psychic &&
-				  pokeInfo?.types[0]?.type.name !== 'psychic' &&
-				  pokeInfo?.types[1]?.type.name !== 'psychic'
-				? false
-				: props.filters.rock &&
-				  pokeInfo?.types[0]?.type.name !== 'rock' &&
-				  pokeInfo?.types[1]?.type.name !== 'rock'
-				? false
-				: props.filters.steel &&
-				  pokeInfo?.types[0]?.type.name !== 'steel' &&
-				  pokeInfo?.types[1]?.type.name !== 'steel'
-				? false
-				: props.filters.water &&
-				  pokeInfo?.types[0]?.type.name !== 'water' &&
-				  pokeInfo?.types[1]?.type.name !== 'water'
-				? false
-				: true
-			: props.filters.bug &&
-			  (pokeInfo?.types[0]?.type.name === 'bug' ||
-					pokeInfo?.types[1]?.type.name === 'bug')
-			? true
-			: props.filters.dark &&
-			  (pokeInfo?.types[0]?.type.name === 'dark' ||
-					pokeInfo?.types[1]?.type.name === 'dark')
-			? true
-			: props.filters.dragon &&
-			  (pokeInfo?.types[0]?.type.name === 'dragon' ||
-					pokeInfo?.types[1]?.type.name === 'dragon')
-			? true
-			: props.filters.electric &&
-			  (pokeInfo?.types[0]?.type.name === 'electric' ||
-					pokeInfo?.types[1]?.type.name === 'electric')
-			? true
-			: props.filters.fairy &&
-			  (pokeInfo?.types[0]?.type.name === 'fairy' ||
-					pokeInfo?.types[1]?.type.name === 'fairy')
-			? true
-			: props.filters.fighting &&
-			  (pokeInfo?.types[0]?.type.name === 'fighting' ||
-					pokeInfo?.types[1]?.type.name === 'fighting')
-			? true
-			: props.filters.fighting &&
-			  (pokeInfo?.types[0]?.type.name === 'fighting' ||
-					pokeInfo?.types[1]?.type.name === 'fighting')
-			? true
-			: props.filters.fire &&
-			  (pokeInfo?.types[0]?.type.name === 'fire' ||
-					pokeInfo?.types[1]?.type.name === 'fire')
-			? true
-			: props.filters.flying &&
-			  (pokeInfo?.types[0]?.type.name === 'flying' ||
-					pokeInfo?.types[1]?.type.name === 'flying')
-			? true
-			: props.filters.ghost &&
-			  (pokeInfo?.types[0]?.type.name === 'ghost' ||
-					pokeInfo?.types[1]?.type.name === 'ghost')
-			? true
-			: props.filters.grass &&
-			  (pokeInfo?.types[0]?.type.name === 'grass' ||
-					pokeInfo?.types[1]?.type.name === 'grass')
-			? true
-			: props.filters.ground &&
-			  (pokeInfo?.types[0]?.type.name === 'ground' ||
-					pokeInfo?.types[1]?.type.name === 'ground')
-			? true
-			: props.filters.ice &&
-			  (pokeInfo?.types[0]?.type.name === 'ice' ||
-					pokeInfo?.types[1]?.type.name === 'ice')
-			? true
-			: props.filters.normal &&
-			  (pokeInfo?.types[0]?.type.name === 'normal' ||
-					pokeInfo?.types[1]?.type.name === 'normal')
-			? true
-			: props.filters.poison &&
-			  (pokeInfo?.types[0]?.type.name === 'poison' ||
-					pokeInfo?.types[1]?.type.name === 'poison')
-			? true
-			: props.filters.psychic &&
-			  (pokeInfo?.types[0]?.type.name === 'psychic' ||
-					pokeInfo?.types[1]?.type.name === 'psychic')
-			? true
-			: props.filters.rock &&
-			  (pokeInfo?.types[0]?.type.name === 'rock' ||
-					pokeInfo?.types[1]?.type.name === 'rock')
-			? true
-			: props.filters.steel &&
-			  (pokeInfo?.types[0]?.type.name === 'steel' ||
-					pokeInfo?.types[1]?.type.name === 'steel')
-			? true
-			: props.filters.water &&
-			  (pokeInfo?.types[0]?.type.name === 'water' ||
-					pokeInfo?.types[1]?.type.name === 'water')
-			? true
-			: !props.filters.bug &&
-			  !props.filters.dark &&
-			  !props.filters.dragon &&
-			  !props.filters.electric &&
-			  !props.filters.fairy &&
-			  !props.filters.fighting &&
-			  !props.filters.fire &&
-			  !props.filters.flying &&
-			  !props.filters.ghost &&
-			  !props.filters.grass &&
-			  !props.filters.ground &&
-			  !props.filters.ice &&
-			  !props.filters.normal &&
-			  !props.filters.poison &&
-			  !props.filters.psychic &&
-			  !props.filters.rock &&
-			  !props.filters.steel &&
-			  !props.filters.water
-			? true
-			: false
-	}
-
-	if (checkFilters()) {
-		return (
-			<div className='m-auto bg-gray-700 px-3 py-3 rounded justify-center text-center my-3 w-40 sm:w-56'>
-				<p className='text-white'>#{pokeInfo?.id}</p>
-				<p className='text-white text-md sm:text-2xl font-semibold sm:font-medium font-mono'>
-					{pokeInfo?.name}
-				</p>
-				<div className='w-full bg-white rounded mb-3 h-20'>
-					<img
-						src={pokeInfo?.sprites.front_default}
-						alt={pokeInfo?.name}
-						className='bg-white m-auto sm:px-10 rounded h-full sm:w-full'
-					/>
-				</div>
-				<div
-					className={
-						'flex justify-center grid gap-x-2 ' +
-						(pokeInfo?.types[1]?.type.name
-							? 'grid-cols-2'
-							: 'grid-cols-1 px-10')
-					}
+	return (
+		<div className='m-auto bg-gray-700 px-3 py-3 rounded justify-center text-center my-3 w-40 sm:w-56'>
+			<p className='text-white'>#{props.pokeInfo?.id}</p>
+			<p className='text-white text-md sm:text-2xl font-semibold sm:font-medium font-mono'>
+				{props.pokeInfo?.name}
+			</p>
+			<div className='w-full bg-white rounded mb-3 h-20'>
+				<img
+					src={props.pokeInfo?.sprites.front_default}
+					alt={props.pokeInfo?.name}
+					className='bg-white m-auto sm:px-10 rounded h-full sm:w-full'
+				/>
+			</div>
+			<div
+				className={
+					'flex justify-center grid gap-x-2 ' +
+					(props.pokeInfo?.types[1]?.type.name
+						? 'grid-cols-2'
+						: 'grid-cols-1 px-10')
+				}
+			>
+				<p
+					className='text-sm  sm:text-xl font-medium px-1 sm:px-2 py-1 text-gray-800 rounded-xl w-full'
+					style={{
+						backgroundColor: getColorType(props.pokeInfo.types[0]?.type.name),
+					}}
 				>
+					{props.pokeInfo.types[0]?.type.name}
+				</p>
+				{props.pokeInfo.types[1]?.type.name ? (
 					<p
 						className='text-sm  sm:text-xl font-medium px-1 sm:px-2 py-1 text-gray-800 rounded-xl w-full'
 						style={{
-							backgroundColor: getColorType(pokeInfo?.types[0]?.type.name),
+							backgroundColor: getColorType(props.pokeInfo.types[1]?.type.name),
 						}}
 					>
-						{pokeInfo?.types[0]?.type.name}
+						{props.pokeInfo.types[1]?.type.name}
 					</p>
-					{pokeInfo?.types[1]?.type.name ? (
-						<p
-							className='text-sm  sm:text-xl font-medium px-1 sm:px-2 py-1 text-gray-800 rounded-xl w-full'
-							style={{
-								backgroundColor: getColorType(pokeInfo?.types[1]?.type.name),
-							}}
-						>
-							{pokeInfo?.types[1]?.type.name}
-						</p>
-					) : (
-						''
-					)}
-				</div>
+				) : (
+					''
+				)}
 			</div>
-		)
-	} else return <></>
+		</div>
+	)
+	//} else return <></>
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -31,30 +31,54 @@ interface Filter {
 	intersection: boolean
 }
 
+const defaultFilter = {
+	bug: false,
+	dark: false,
+	dragon: false,
+	electric: false,
+	fairy: false,
+	fighting: false,
+	fire: false,
+	flying: false,
+	ghost: false,
+	grass: false,
+	ground: false,
+	ice: false,
+	normal: false,
+	poison: false,
+	psychic: false,
+	rock: false,
+	steel: false,
+	water: false,
+	intersection: false,
+}
+
+interface Pokemon {
+	id: number
+	name: string
+	types: [
+		{
+			type: {
+				name: string
+			}
+		},
+		{
+			type: {
+				name: string
+			}
+		}
+	]
+	sprites: {
+		front_default: string
+	}
+}
+
 export const Home = () => {
 	const [data, setData] = useState('')
 	const [allPokes, setAllPokes] = useState<Poke[]>([])
-	const [filters, setFilters] = useState<Filter>({
-		bug: false,
-		dark: false,
-		dragon: false,
-		electric: false,
-		fairy: false,
-		fighting: false,
-		fire: false,
-		flying: false,
-		ghost: false,
-		grass: false,
-		ground: false,
-		ice: false,
-		normal: false,
-		poison: false,
-		psychic: false,
-		rock: false,
-		steel: false,
-		water: false,
-		intersection: false,
-	})
+	const [filters, setFilters] = useState<Filter>(defaultFilter)
+	const [pokesInfo, setPokesInfo] = useState<Pokemon[]>([])
+	const [pokeIndex, setPokeIndex] = useState(0)
 
 	useEffect(() => {
 		axios
@@ -67,8 +91,215 @@ export const Home = () => {
 			})
 	}, [])
 
-	const childSearchToParent = (pokesearch: string): void => {
+	useEffect(() => {
+		console.log('update')
+	}, [pokesInfo])
+
+	const checkFilters = (pokeInfo: Pokemon) => {
+		return filters.intersection
+			? filters.bug &&
+			  pokeInfo?.types[0]?.type.name !== 'bug' &&
+			  pokeInfo?.types[1]?.type.name !== 'bug'
+				? false
+				: filters.dark &&
+				  pokeInfo?.types[0]?.type.name !== 'dark' &&
+				  pokeInfo?.types[1]?.type.name !== 'dark'
+				? false
+				: filters.dragon &&
+				  pokeInfo?.types[0]?.type.name !== 'dragon' &&
+				  pokeInfo?.types[1]?.type.name !== 'dragon'
+				? false
+				: filters.electric &&
+				  pokeInfo?.types[0]?.type.name !== 'electric' &&
+				  pokeInfo?.types[1]?.type.name !== 'electric'
+				? false
+				: filters.fairy &&
+				  pokeInfo?.types[0]?.type.name !== 'fairy' &&
+				  pokeInfo?.types[1]?.type.name !== 'fairy'
+				? false
+				: filters.fighting &&
+				  pokeInfo?.types[0]?.type.name !== 'fighting' &&
+				  pokeInfo?.types[1]?.type.name !== 'fighting'
+				? false
+				: filters.fighting &&
+				  pokeInfo?.types[0]?.type.name !== 'fighting' &&
+				  pokeInfo?.types[1]?.type.name !== 'fighting'
+				? false
+				: filters.fire &&
+				  pokeInfo?.types[0]?.type.name !== 'fire' &&
+				  pokeInfo?.types[1]?.type.name !== 'fire'
+				? false
+				: filters.flying &&
+				  pokeInfo?.types[0]?.type.name !== 'flying' &&
+				  pokeInfo?.types[1]?.type.name !== 'flying'
+				? false
+				: filters.ghost &&
+				  pokeInfo?.types[0]?.type.name !== 'ghost' &&
+				  pokeInfo?.types[1]?.type.name !== 'ghost'
+				? false
+				: filters.grass &&
+				  pokeInfo?.types[0]?.type.name !== 'grass' &&
+				  pokeInfo?.types[1]?.type.name !== 'grass'
+				? false
+				: filters.ground &&
+				  pokeInfo?.types[0]?.type.name !== 'ground' &&
+				  pokeInfo?.types[1]?.type.name !== 'ground'
+				? false
+				: filters.ice &&
+				  pokeInfo?.types[0]?.type.name !== 'ice' &&
+				  pokeInfo?.types[1]?.type.name !== 'ice'
+				? false
+				: filters.normal &&
+				  pokeInfo?.types[0]?.type.name !== 'normal' &&
+				  pokeInfo?.types[1]?.type.name !== 'normal'
+				? false
+				: filters.poison &&
+				  pokeInfo?.types[0]?.type.name !== 'poison' &&
+				  pokeInfo?.types[1]?.type.name !== 'poison'
+				? false
+				: filters.psychic &&
+				  pokeInfo?.types[0]?.type.name !== 'psychic' &&
+				  pokeInfo?.types[1]?.type.name !== 'psychic'
+				? false
+				: filters.rock &&
+				  pokeInfo?.types[0]?.type.name !== 'rock' &&
+				  pokeInfo?.types[1]?.type.name !== 'rock'
+				? false
+				: filters.steel &&
+				  pokeInfo?.types[0]?.type.name !== 'steel' &&
+				  pokeInfo?.types[1]?.type.name !== 'steel'
+				? false
+				: filters.water &&
+				  pokeInfo?.types[0]?.type.name !== 'water' &&
+				  pokeInfo?.types[1]?.type.name !== 'water'
+				? false
+				: true
+			: filters.bug &&
+			  (pokeInfo?.types[0]?.type.name === 'bug' ||
+					pokeInfo?.types[1]?.type.name === 'bug')
+			? true
+			: filters.dark &&
+			  (pokeInfo?.types[0]?.type.name === 'dark' ||
+					pokeInfo?.types[1]?.type.name === 'dark')
+			? true
+			: filters.dragon &&
+			  (pokeInfo?.types[0]?.type.name === 'dragon' ||
+					pokeInfo?.types[1]?.type.name === 'dragon')
+			? true
+			: filters.electric &&
+			  (pokeInfo?.types[0]?.type.name === 'electric' ||
+					pokeInfo?.types[1]?.type.name === 'electric')
+			? true
+			: filters.fairy &&
+			  (pokeInfo?.types[0]?.type.name === 'fairy' ||
+					pokeInfo?.types[1]?.type.name === 'fairy')
+			? true
+			: filters.fighting &&
+			  (pokeInfo?.types[0]?.type.name === 'fighting' ||
+					pokeInfo?.types[1]?.type.name === 'fighting')
+			? true
+			: filters.fighting &&
+			  (pokeInfo?.types[0]?.type.name === 'fighting' ||
+					pokeInfo?.types[1]?.type.name === 'fighting')
+			? true
+			: filters.fire &&
+			  (pokeInfo?.types[0]?.type.name === 'fire' ||
+					pokeInfo?.types[1]?.type.name === 'fire')
+			? true
+			: filters.flying &&
+			  (pokeInfo?.types[0]?.type.name === 'flying' ||
+					pokeInfo?.types[1]?.type.name === 'flying')
+			? true
+			: filters.ghost &&
+			  (pokeInfo?.types[0]?.type.name === 'ghost' ||
+					pokeInfo?.types[1]?.type.name === 'ghost')
+			? true
+			: filters.grass &&
+			  (pokeInfo?.types[0]?.type.name === 'grass' ||
+					pokeInfo?.types[1]?.type.name === 'grass')
+			? true
+			: filters.ground &&
+			  (pokeInfo?.types[0]?.type.name === 'ground' ||
+					pokeInfo?.types[1]?.type.name === 'ground')
+			? true
+			: filters.ice &&
+			  (pokeInfo?.types[0]?.type.name === 'ice' ||
+					pokeInfo?.types[1]?.type.name === 'ice')
+			? true
+			: filters.normal &&
+			  (pokeInfo?.types[0]?.type.name === 'normal' ||
+					pokeInfo?.types[1]?.type.name === 'normal')
+			? true
+			: filters.poison &&
+			  (pokeInfo?.types[0]?.type.name === 'poison' ||
+					pokeInfo?.types[1]?.type.name === 'poison')
+			? true
+			: filters.psychic &&
+			  (pokeInfo?.types[0]?.type.name === 'psychic' ||
+					pokeInfo?.types[1]?.type.name === 'psychic')
+			? true
+			: filters.rock &&
+			  (pokeInfo?.types[0]?.type.name === 'rock' ||
+					pokeInfo?.types[1]?.type.name === 'rock')
+			? true
+			: filters.steel &&
+			  (pokeInfo?.types[0]?.type.name === 'steel' ||
+					pokeInfo?.types[1]?.type.name === 'steel')
+			? true
+			: filters.water &&
+			  (pokeInfo?.types[0]?.type.name === 'water' ||
+					pokeInfo?.types[1]?.type.name === 'water')
+			? true
+			: !filters.bug &&
+			  !filters.dark &&
+			  !filters.dragon &&
+			  !filters.electric &&
+			  !filters.fairy &&
+			  !filters.fighting &&
+			  !filters.fire &&
+			  !filters.flying &&
+			  !filters.ghost &&
+			  !filters.grass &&
+			  !filters.ground &&
+			  !filters.ice &&
+			  !filters.normal &&
+			  !filters.poison &&
+			  !filters.psychic &&
+			  !filters.rock &&
+			  !filters.steel &&
+			  !filters.water
+			? true
+			: false
+	}
+
+	const childSearchToParent = (
+		pokesearch: string,
+		init: number = pokeIndex,
+		end: number = pokeIndex + 12
+	) => {
+		setPokeIndex(0)
 		setData(pokesearch)
+		setPokesInfo([])
+		const namesPokes = allPokes.filter((poke) => {
+			return poke.name.includes(pokesearch)
+		})
+		loadPokes(namesPokes, 0, end)
+		console.log('oi')
+	}
+
+	const loadPokes = (
+		pokes: Poke[],
+		i: number = pokeIndex,
+		f: number = pokeIndex + 12
+	) => {
+		pokes.slice(i, f).map(async (poke) => {
+			const res = await axios.get(poke.url)
+			if (checkFilters(res.data)) {
+				//qaaa.push(res.data)
+				setPokesInfo((pokesInfo) => [...pokesInfo, res.data])
+			}
+		})
+		setPokeIndex(pokeIndex + 12)
 	}
 
 	const childFilterToParent = (childFilters: Filter): void => {
@@ -79,20 +310,25 @@ export const Home = () => {
 			<SearchBar childSearchToParent={childSearchToParent} />
 			<FilterBar childFilterToParent={childFilterToParent} />
 			<div className='grid grid-cols-2 md:grid-cols-3 w-full max-w-4xl m-auto bg-white px-0 sm:px-4 md:px-10'>
-				{allPokes
-					.filter((poke) => {
-						return data.trim() !== '' ? poke.name.includes(data) : false
-					})
-					.map((poke, index) => {
-						return (
-							<PokeBox
-								name={poke.name}
-								url={poke.url}
-								filters={filters}
-								key={poke.name}
-							/>
-						)
+				{pokesInfo
+					.sort((a, b) => (a.id > b.id ? 1 : -1))
+					.map((poke) => {
+						return <PokeBox key={poke.id} pokeInfo={poke} />
 					})}
+			</div>
+			<div className='bg-gray-600 flex'>
+				<input
+					type='button'
+					value='More'
+					onClick={() => {
+						loadPokes(
+							allPokes.filter((poke) => {
+								return poke.name.includes(data)
+							})
+						)
+					}}
+					className='m-auto focus cursor-pointer'
+				/>
 			</div>
 		</>
 	)
